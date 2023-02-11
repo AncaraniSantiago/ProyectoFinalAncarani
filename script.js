88d88f2080bd82951692aba4237aa30cc6a3cb16
Ancarani <santiancarani@gmail.com>
2023-02-11 03:46:41 -0300

    Aplicando fech, y mejorando funciones

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-var arrCarro=[]
-if (localStorage.getItem("carrito")) {
-  arrCarro = JSON.parse(localStorage.getItem("carrito"))
-}
-const arrProductos=[
-producto1={id:Number(1), nombre: "Jalea Ambar", categoria: "(Env. descartable)",moneda:"$", precio:Number(2380), stock:Number(20), imgUrl:"https://i.postimg.cc/zHXpktcj/jalAmb.png"},
-producto2={id:Number(2), nombre: "Jalea Ambar", categoria: "(Env. Retornable)",moneda:"$", precio:Number(1890), stock:Number(10), imgUrl:"https://i.postimg.cc/qgj17rNZ/jal-Amb-Reto.png"},
-producto3={id:Number(3), nombre: "Jalea Carmesi", categoria: "(Env. descartable)",moneda:"$", precio:Number(1870), stock:Number(1), imgUrl:"https://i.postimg.cc/Jt1Tm0zk/jalCarme.png"}
-];
-
-localStorage.setItem("productos", JSON.stringify(arrProductos))
-programa(JSON.parse(localStorage.getItem("productos")))
+fetch("./data.json")
+.then(resp => resp.json())
+.then(data => programa(data))
+.catch(err => console.log(err))
 
 function programa(arrProductos) {
+  var arrCarro=[]
+
+  if (localStorage.getItem("carrito")) {
+    arrCarro = JSON.parse(localStorage.getItem("carrito"))
+    console.log(arrCarro);
+  }
+  if (arrCarro.length !=0) {
+    agregaBt("compraron","Confirmar compra",comprarOn)
+    agregaBt("mostCarr","Mostrar Carrito",verCarro1)
+  }
   let logo = document.getElementById("logo")
   logo.innerHTML='<img src=https://i.postimg.cc/kG72b57s/logo-Surat.png>'
   let sectionProd = document.getElementById("secProd")
@@ -19,11 +21,8 @@ function programa(arrProductos) {
   buscador.onchange = filtrar
   let buscar = document.getElementById("buscar")
   buscar.onclick = filtrar
-  let compraron = document.getElementById("compraron")
-  compraron.onclick = comprarOn
-  let mostCarro = document.getElementById("mostCarr")
-  mostCarro.onclick = verCarro1
-  let mostTodos = document.getElementById("mostTodos")
+  
+  let mostTodos = document.getElementById("logo")
   mostTodos.onclick = todosProductos
   todosProductos()
 
@@ -31,13 +30,13 @@ function programa(arrProductos) {
     renderTarjetas(arrProductos)
   }
   function filtrar() {
-    let productosFiltrados = arrProductos.filter(producto => producto.nombre.toLocaleUpperCase().includes(buscador.value.toLocaleUpperCase())||producto.categoria.toLocaleUpperCase().includes(buscador.value.toLocaleUpperCase()))
+    let productosFiltrados = arrProductos.filter(producto => producto.nombre.toUpperCase().includes(buscador.value.toUpperCase())||producto.categoria.toUpperCase().includes(buscador.value.toUpperCase()))
     renderTarjetas(productosFiltrados)
   }
-  function renderTarjetas(arrProductosRender) {
+  function renderTarjetas(arrProductos) {
     sectionProd.innerHTML=""
-    arrProductosRender.forEach(producto => {
-      if(producto.stock > Number(0)){
+    arrProductos.forEach(producto => {
+      if(producto.stock > 0){
         let tarjetaProducto = document.createElement("div")
         tarjetaProducto.classList.add("tarjeta")
         tarjetaProducto.innerHTML=`
@@ -47,7 +46,7 @@ function programa(arrProductos) {
         <button id=${producto.id} class="bt">Agregar al carro</button>
         `
         if(producto.stock < 5){
-          tarjetaProducto.classList.add("bajoStock")
+          tarjetaProducto.classList.replace("tarjeta","bajoStock")
           let stockBajo = document.createElement("p")
           stockBajo.classList.add("ultimas")
           if (producto.stock == 1 ){
@@ -63,29 +62,38 @@ function programa(arrProductos) {
       }
     })
   }
-  function agregaCarro(e) {
-    let id = e.target.id
-    let productoBuscado = arrProductos.find(producto => producto.id == id)
-    Toastify({
-      text: "Producto agregado a tu carrito",
-      className: "info",
-      offset: {
-        x: 30,
-        y: 63,
-      },
-      style: {
-        background: "#F2B705",
-      }
-    }).showToast();
+  function agregaCarro(e) { 
+    let productoBuscado = arrProductos.find(producto => producto.id == e.target.id)
+    console.log(arrCarro);
+    if (arrCarro.length===0) {
+      agregaBt("compraron","Confirmar compra",comprarOn)
+      agregaBt("mostCarr","Mostrar Carrito",verCarro1)
+    }
     if (arrCarro.indexOf(productoBuscado) == -1) {
+      console.log(arrCarro.indexOf(productoBuscado));
       arrCarro.push(productoBuscado);
-      productoBuscado.cantidad = Number(1)
-      productoBuscado.total = Number(productoBuscado.cantidad*productoBuscado.precio)
+      let prodCarro = arrCarro.find(producto => producto.id == e.target.id)
+      prodCarro.cantidad = 1
+      prodCarro.total = productoBuscado.cantidad*productoBuscado.precio
+      toasty("Unidad agregada","#F2B705")
     }
     else if (arrCarro.indexOf(productoBuscado) != -1 && productoBuscado.cantidad<productoBuscado.stock){
       productoBuscado.cantidad++
+      toasty("Unidad agregada","#F2B705")
     }
-      localStorage.setItem("carrito", JSON.stringify(arrCarro))
+    else{
+      toasty("Maxima cantidad cargada! (stock: "+productoBuscado.stock+")","rgb(188, 0, 0)")
+    }
+    localStorage.setItem("carrito", JSON.stringify(arrCarro))
+  }
+  function agregaBt(idBt, text, func) {
+    let btCompra = document.createElement("button")
+    btCompra.id=(idBt)
+    btCompra.innerHTML=text
+    let botones = document.getElementById("botones")
+    botones.append(btCompra)
+    let bt = document.getElementById(idBt)
+    bt.onclick = func
   }
   function verCarro1() {
     sectionProd.innerHTML=""
@@ -98,42 +106,53 @@ function programa(arrProductos) {
       <p>Cantidad: ${Number(producto.cantidad)}</p>
       <p>Total:</p>
       <p>${producto.moneda+(producto.precio*producto.cantidad)}</p>
-      <button id=${producto.id} class="bt" onclick=quitarCarro(${producto.id})>Eliminar del carrito</button>
+      <button id=${producto.id} class="bt" onclick=quitarCarro(${producto.id})>Quitar del carrito</button>
       `
       sectionProd.append(tarjetaProducto)
       let quitAcarro = document.getElementById(producto.id)
-        quitAcarro.onclick = quitarCarro
+      quitAcarro.onclick = quitarCarro
+
     })
   }
   function quitarCarro(e) {
-    console.log(e.target.id);
     posProdBuscado = arrCarro.findIndex(producto => producto.id == e.target.id)
     if(arrCarro[posProdBuscado].cantidad > 1){
       arrCarro[posProdBuscado].cantidad--
+      toasty("Unidad removida","rgb(188, 0, 0)")
     }
     else {
       arrCarro.splice(posProdBuscado, 1)
+      toasty("Producto eliminado","rgb(158, 0, 0)")
     }
-    localStorage.setItem("carrito", JSON.stringify(arrCarro))
+    localStorage.setItem("carrito", JSON.stringify(arrCarro))    
     verCarro1(arrCarro)
+    if (arrCarro.length===0) {
+      quitaBt("compraron")
+      quitaBt("mostCarr")
+      todosProductos()
+    }
   }
+  function quitaBt(idBoton) {
+    botones = document.getElementById("botones")
+    bt = document.getElementById(idBoton)
+    botones.removeChild(bt);
+}
   function comprarOn() {
     final()
     arrCarro.forEach(producto => {
       quitaStock(producto.id)
-      localStorage.setItem("productos", JSON.stringify(arrProductos))
     })
     arrCarro = []
+    quitaBt("compraron")
+    quitaBt("mostCarr")
     localStorage.removeItem("carrito")
     todosProductos()
   }
   function quitaStock(idq){
-    arrCarro.forEach(producto => {
-      let productoGral = arrProductos.find(producto => producto.id == idq)
-        if((producto.cantidad) <= productoGral.stock){productoGral.stock=(productoGral.stock)-(producto.cantidad)
-        arrCarro.shift(producto)
-      }
-    })
+    let productoGral = arrProductos.find(productoBd => productoBd.id === idq)
+    let prodCarro = arrCarro.find(producto => producto.id === idq)
+    if(prodCarro.cantidad <= productoGral.stock){productoGral.stock=(productoGral.stock)-(prodCarro.cantidad)}
+    else{alert("Vaciar el carro antes de actualizar")}
   }
   function final() {
     let tot=Number()
@@ -141,5 +160,18 @@ function programa(arrProductos) {
       tot += (producto.cantidad*producto.precio)
     })
     alert("Gracias por su compra\n\nEl total a abonar es de: $ "+tot)
+  }
+  function toasty(text,background) {
+    Toastify({
+      text,
+      className: "info",
+      offset: {
+        x: 30,
+        y: 30,
+      },
+      style: {
+        background,
+      }
+    }).showToast();
   }
 }
