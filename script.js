d4bb5d62bcc6ee1f6a5e1dcfd6540ae75caaaf60
Ancarani <santiancarani@gmail.com>
2023-02-13 17:19:38 -0300

    Mejorando navegación

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ function programa(arrProductos) {
 
   if (localStorage.getItem("carrito")) {
     arrCarro = JSON.parse(localStorage.getItem("carrito"))
-    console.log(arrCarro);
   }
   if (arrCarro.length !=0) {
     agregaBt("compraron","Confirmar compra",comprarOn)
@@ -53,24 +52,31 @@ function programa(arrProductos) {
             stockBajo.innerText="Ultima unidad!"
           }
           else{
-          stockBajo.innerText="Ultimas unidades!"}
-          tarjetaProducto.appendChild(stockBajo)
+            stockBajo.innerText="Ultimas unidades!"}
+            tarjetaProducto.appendChild(stockBajo)
+          }
+          sectionProd.append(tarjetaProducto)
+          let agrAcarro = document.getElementById(producto.id)
+          agrAcarro.onclick = agregaCarro
         }
-        sectionProd.append(tarjetaProducto)
-        let agrAcarro = document.getElementById(producto.id)
-        agrAcarro.onclick = agregaCarro
+      })
+      tot()
+      if (document.getElementById("mostTodos")) {
+        quitaBt("mostTodos")
+        agregaBt("mostCarr","Mostrar Carrito",verCarro1)
       }
-    })
+      if (document.getElementById("mostCarr")&&arrCarro.length===0) {
+        quitaBt("mostCarr")
+        quitaBt("compraron")
+      }
   }
   function agregaCarro(e) { 
     let productoBuscado = arrProductos.find(producto => producto.id == e.target.id)
-    console.log(arrCarro);
     if (arrCarro.length===0) {
       agregaBt("compraron","Confirmar compra",comprarOn)
       agregaBt("mostCarr","Mostrar Carrito",verCarro1)
     }
     if (arrCarro.indexOf(productoBuscado) == -1) {
-      console.log(arrCarro.indexOf(productoBuscado));
       arrCarro.push(productoBuscado);
       let prodCarro = arrCarro.find(producto => producto.id == e.target.id)
       prodCarro.cantidad = 1
@@ -85,15 +91,7 @@ function programa(arrProductos) {
       toasty("Maxima cantidad cargada! (stock: "+productoBuscado.stock+")","rgb(188, 0, 0)")
     }
     localStorage.setItem("carrito", JSON.stringify(arrCarro))
-  }
-  function agregaBt(idBt, text, func) {
-    let btCompra = document.createElement("button")
-    btCompra.id=(idBt)
-    btCompra.innerHTML=text
-    let botones = document.getElementById("botones")
-    botones.append(btCompra)
-    let bt = document.getElementById(idBt)
-    bt.onclick = func
+    tot()
   }
   function verCarro1() {
     sectionProd.innerHTML=""
@@ -111,55 +109,91 @@ function programa(arrProductos) {
       sectionProd.append(tarjetaProducto)
       let quitAcarro = document.getElementById(producto.id)
       quitAcarro.onclick = quitarCarro
-
     })
+    if(document.getElementById("mostCarr")){
+      quitaBt("mostCarr")
+      agregaBt("mostTodos","Todos los productos",todosProductos)
+    }
+    if (arrCarro.length===0) {
+    quitaBt("mostCarr")
+    todosProductos()
+    }
+    tot()
   }
+
   function quitarCarro(e) {
     posProdBuscado = arrCarro.findIndex(producto => producto.id == e.target.id)
     if(arrCarro[posProdBuscado].cantidad > 1){
       arrCarro[posProdBuscado].cantidad--
       toasty("Unidad removida","rgb(188, 0, 0)")
+      tot()
     }
     else {
       arrCarro.splice(posProdBuscado, 1)
       toasty("Producto eliminado","rgb(158, 0, 0)")
+      tot()
     }
     localStorage.setItem("carrito", JSON.stringify(arrCarro))    
-    verCarro1(arrCarro)
     if (arrCarro.length===0) {
-      quitaBt("compraron")
-      quitaBt("mostCarr")
+      // quitaBt("compraron")
+      // quitaBt("mostCarr")
       todosProductos()
     }
+    else{
+    verCarro1(arrCarro)
+    tot()}
+  }
+  function comprarOn() {
+    let tot = final()
+    total(tot)
+    arrCarro.forEach(producto => {
+      quitaStock(producto.id)
+    })
+    arrCarro = []
+    localStorage.removeItem("carrito")
+    todosProductos()
+  }
+  function agregaBt(idBt, text, func) {
+    let btCompra = document.createElement("button")
+    btCompra.id=(idBt)
+    btCompra.innerHTML=text
+    let botones = document.getElementById("botones")
+    botones.append(btCompra)
+    let bt = document.getElementById(idBt)
+    bt.onclick = func
   }
   function quitaBt(idBoton) {
     botones = document.getElementById("botones")
     bt = document.getElementById(idBoton)
     botones.removeChild(bt);
 }
-  function comprarOn() {
-    final()
-    arrCarro.forEach(producto => {
-      quitaStock(producto.id)
-    })
-    arrCarro = []
-    quitaBt("compraron")
-    quitaBt("mostCarr")
-    localStorage.removeItem("carrito")
-    todosProductos()
+  function mostTotal(tot){
+    let info = document.getElementById("info")
+    info.innerHTML=""
+    let textInfo = document.createElement("div")
+    textInfo.innerHTML=`
+    <p>El total de su carrito suma : $${tot}</p>`
+    info.append(textInfo)
   }
-  function quitaStock(idq){
-    let productoGral = arrProductos.find(productoBd => productoBd.id === idq)
-    let prodCarro = arrCarro.find(producto => producto.id === idq)
-    if(prodCarro.cantidad <= productoGral.stock){productoGral.stock=(productoGral.stock)-(prodCarro.cantidad)}
-    else{alert("Vaciar el carro antes de actualizar")}
+  function total(tot) {
+    alert("Gracias por su compra\n\nEl total a abonar es de: $ "+tot)
   }
   function final() {
     let tot=Number()
     arrCarro.forEach(producto => {
       tot += (producto.cantidad*producto.precio)
     })
-    alert("Gracias por su compra\n\nEl total a abonar es de: $ "+tot)
+    return tot
+  }
+  function tot() {
+    let tot = final()
+    mostTotal(tot)
+  }
+  function quitaStock(idq){
+    let productoGral = arrProductos.find(productoBd => productoBd.id === idq)
+    let prodCarro = arrCarro.find(producto => producto.id === idq)
+    if(prodCarro.cantidad <= productoGral.stock){productoGral.stock=(productoGral.stock)-(prodCarro.cantidad)}
+    else{alert("Confirmar compra, conflicto de stock renovado al actualizar")}
   }
   function toasty(text,background) {
     Toastify({
